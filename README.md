2b9695ab7e7c1f9774554689f6edad0f3d6fb6bc
george <george>
2018-01-10 07:43:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+
+osmocat.py
+==========
+
+This repo provides a Python (2.7) gnuradio script named osmocat.py.
+The script sends samples from any gr-osmosdr supported device to 
+standard output.  I created the script namely to connect my AirspyHF+ radio to 
+the Openwebrx software receiver.  At the time of this writing only 
+SDR# and gr-osmosdr clients support the AirspyHF+.
+
+To use Openwebrx with a particular SDR radio, a command line
+program that can write samples to standard output is required.
+For example to listen to a RTL-SDR, Openwebrx can use the
+program rtl\_sdr.  In other words osmocat.py is like rtl\_sdr but for
+gr-osmosdr devices. 
+
+To see the script's help screen run:
+
+    $ python osmocat.py -h
+
+    usage: osmocat.py [-h] [--args ARGS] [--freq FREQ] [--rate RATE] [--corr CORR]
+                      [--gain GAIN] [--mode MODE]
+
+    optional arguments:
+      -h, --help   show this help message and exit
+      --args ARGS  device arguments
+      --freq FREQ  center frequency (Hz)
+      --rate RATE  sample rate (Hz)
+      --corr CORR  freq correction (ppm)
+      --gain GAIN  gain (dB)
+      --mode MODE  gain mode (0 or 1)
+
+Setting the gain mode to 1 turns on automatic gain.
+
+To use osmocat.py with Openwebrx add the following lines at
+the end of config\_webrx.py:
+
+    start_rtl_command=("python osmocat.py "
+                       "--args '{device_args}' "
+                       "--mode {gain_mode} "
+                       "--freq {center_freq} "
+                       "--rate {samp_rate}").format(
+        center_freq=center_freq, 
+        samp_rate=samp_rate, 
+        gain_mode=gain_mode, 
+        device_args=device_args)
+    format_conversion=""
+
+So, for example, to listen to a RTL-SDR device, change the following variables
+in config\_webrx.py to:
+
+    device_args = "rtl=0"
+    gain_mode = 1
+    samp_rate = 2400000
+    center_freq = 470000000
+
+Since a blank device argument picks up the first
+gr-osmosdr radio found, device\_args can be set to:
+
+    device_args = ""
+
+Now to listen to your AirspyHF+, change the variables, for example, to:
+
+    device_args = "airspyhf=0"
+    gain_mode = 0
+    samp_rate = 768000
+    center_freq = 1000000
+
+The AirspyHF+ gr-osmosdr device only supports a sample rate of 768000.
+It also does not support, as I found out, "--mode" or "--gain", which it
+simply ignores.  Note, if the waterfall on Openwebrx is all black, click
+the auto-adjust waterfall colors button to rescale.  The button looks 
+like a mountain range landscape. 
+ 
+
+- (c) copyright 2018 roseengineering
---
+++ osmocat.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from gnuradio import gr
+from gnuradio import blocks
+import osmosdr
+import sys, struct, argparse
+
+
+class queue_sink(gr.hier_block2):
+
+    def __init__(self):
+        item_size = gr.sizeof_gr_complex
+        gr.hier_block2.__init__(self, "queue_sink",
+            gr.io_signature(1, 1, item_size),
+            gr.io_signature(0, 0, 0))
+        self.qu = gr.msg_queue(0)
+        message_sink = blocks.message_sink(item_size, self.qu, False)
+        self.connect(self, message_sink)
+        self.arr = None
+        self.idx = 0
+		
+    def next(self):
+        msg = self.qu.delete_head()
+        return msg.to_string()
+
+	
+class radio_stream(gr.top_block):
+
+    def __init__(self, **kw):
+        gr.top_block.__init__(self)
+        self.source = source = osmosdr.source(kw['args'])
+        self.sink = sink = queue_sink()
+        self.valid_ranges('valid sampling rates', source.get_sample_rates())
+        self.valid_ranges('valid gains', source.get_gain_range())
+        self.valid_ranges('valid frequencies', source.get_freq_range())
+        source.set_center_freq(int(kw['freq'] or 100e6))
+        if kw.get('rate') != None: source.set_sample_rate(kw['rate'])
+        if kw.get('corr') != None: source.set_freq_corr(kw['corr'])
+        if kw.get('gain') != None: source.set_gain(kw['gain'])
+        if kw.get('mode') != None: source.set_gain_mode(bool(kw['mode']))
+        self.connect(source, sink)
+  
+    def status(self):
+        source = self.source 
+        print('rate = %d' % source.get_sample_rate(), file=sys.stderr)
+        print('freq = %d' % source.get_center_freq(), file=sys.stderr)
+        print('corr = %d' % source.get_freq_corr(), file=sys.stderr)
+        print('gain = %d' % source.get_gain(), file=sys.stderr)
+        print('mode = %s' % source.get_gain_mode(), file=sys.stderr)
+
+    def valid_ranges(self, name, r):
+        print(name + ":", end="", file=sys.stderr)
+        if r.empty():
+            print(" <none>", file=sys.stderr)
+            return
+        for x in r:
+            start = x.start()
+            stop = x.stop()
+            if start == stop: 
+                print(" %d" % start, end="", file=sys.stderr)
+            else:
+                print(" %d-%d" % (start, stop), end="", file=sys.stderr)
+        print(file=sys.stderr)
+
+    def __iter__(self):
+        return self.sink
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--args", help="device arguments", default="")
+    parser.add_argument("--freq", help="center frequency (Hz)", type=float)
+    parser.add_argument("--rate", help="sample rate (Hz)", type=float)
+    parser.add_argument("--corr", help="freq correction (ppm)", type=float)
+    parser.add_argument("--gain", help="gain (dB)", type=float)
+    parser.add_argument("--mode", help="gain mode (0 or 1)", type=int)
+    args = parser.parse_args()
+
+    stream = radio_stream(**args.__dict__)
+    stream.status()
+    stream.start()
+
+    for c in stream:
+        sys.stdout.write(c)
+
